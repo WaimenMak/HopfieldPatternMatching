6c1c2c2a6ff63012c1a6159d15165b39e26ce5cc
WaimenMak <1327149492@qq.com>
2021-03-03 21:15:02 +0800

    divide

---
+++ LAHN_Ver1/Generate_data.m
@@ -0,0 +1,65 @@
+ %% Synthetic data
+ function Syn_P = Generate_data(number,m, N,q_num, PT,nl)   %N:number of stored patterns, number: test set size of one pattern like H&S, m:scaling size
+    Syn_P = zeros(number,m, N);                             %nl: noise level;
+    for n = 1:N
+        for n2 = 1:number
+            Syn_P(n2,:,n) = Synthetic_data(m, q_num, PT(n,:),nl);
+        end
+    end
+    
+end
+ 
+function P_warp = Synthetic_data(m, q_num, PT, nl) %PT:predefined pattern (PIP data)
+
+% PT = [0, 0.7, 0.3, 1, 0.3, 0.7, 0;
+%       0.1, 0.3, 0.5, 0.5, 0.5,0.8, 1;
+%       0, 1, 0.3, 1, 0.3, 1, 0;
+%       0, 0.7, 0.4, 0.8, 0.6, 0.9 1];
+
+    
+x = 1:q_num;
+% m = 49;       %91,133
+n = q_num;
+
+P_warp = Cre_data(m, n, x, PT, q_num,nl);
+
+% plot(x_warp,P_warp,'-*')
+% figure(2)
+% plot(x,PT,'-*')
+end
+
+function P = Cre_data(m, n, x, PT, q_num,noise_level)
+    %% Time Scaling
+    X = (m - n)/(n - 1);
+    for i = 2:q_num
+        x(i) = x(i - 1) + (X + 1);
+    end
+%     plot(x,PT(1,:),'-*')
+
+    %% Time warping
+    for j = 2:q_num -1
+        x(j) =  round(x(j)-(x(j) - x(j-1))*2/3 + ((x(j)+(x(j+1) - x(j))*2/3) - (x(j)-(x(j) - x(j-1))*2/3))*rand(1));
+%         x(j) =  round(x(j)-(x(j) - x(j-1))*2.5/3 + ((x(j)+(x(j+1) - x(j))*2.5/3) - (x(j)-(x(j) - x(j-1))*2.5/3))*rand(1));
+
+    end
+
+
+    %% Time Scaling
+
+    P = zeros(1, m);
+    for c = 1:q_num - 1
+        j = 0;
+        for i = x(c):x(c+1)
+            P(i) = PT(c) + j * (PT(c+1) - PT(c))/(x(c+1) - x(c));
+            j = j+1;
+        end
+    end
+    
+    %% Noise Adding
+    for u = 1:m - 1
+        if rand < 0.7
+            P(u) = P(u) + normrnd(0,noise_level);   %sigma = 0.05  noise level  0.1:high 0.05:low
+        end
+    end
+   
+end
---
+++ LAHN_Ver1/asymmetric_train.m
@@ -0,0 +1,79 @@
+%% asymmetric hopfield net
+Func = Utils;
+neuron_num = 7*7;
+X = zeros(N,neuron_num);
+k = 6;
+for i = 1:N
+    p = reshape(Template.represent1(:,:,i),[neuron_num, 1]);
+    X(i,:) = p;
+end
+
+% X = [1.5 -1.3 -1.8 1.5 1.3 1.8 -1.2 0.9 1.7;
+%     -1.7 1.6 -2 -1.5 1.5 -1.8 1.3 -1.7 -1.9;
+%     -2.0 1.5 1.4 1.9 -1.5 -1.9 1.3 -1.3 1.6;
+%     1.6 1.5 -1.8 1.4 -1.6 1.9 -1.8 1.4 1.8];
+
+
+% X= [-0.8, 0.8, 0.8, 0.8, 0.8, -0.8, -0.8, 0.8;
+%     0.8, -0.8, 0.8, -0.8, 0.8, 0.8, 0.8,-0.8;
+%     0.8, 0.8, -0.8, -0.8, 0.8, 0.8, -0.8, -0.8;
+%     -0.8, -0.8, 0.8, -0.8, -0.8, 0.8, 0.8, -0.8];
+
+% ind = X == -1;
+% X(ind) = -2;
+% ind2 = X == 1;
+% X(ind2) = 2;
+
+weight = -1 + 2*rand(size(X,2),size(X,2));  %range(-1,1)
+for j = 1:size(X,2)
+    weight(j,j) = -15;
+end
+
+bias = -1 + 2*rand(size(X,2),1);
+v = 5*ones(1,size(X,2));
+% v = [1.2,0.9,0.5, 1.4, 1.0, 1.5, 2.0, 1.8, 2.0];
+A = diag(v);
+
+iter = 500;
+for t = 1:iter
+    for i = 1:size(X,1)
+        opt = forward(X(i,:),weight,bias,Func,k);
+        y = A*X(i,:)';
+        [weight,bias] = backprob(weight,bias,X(i,:),opt,y,Func,k);
+    end
+    loss = 0;
+    for item = 1:size(X,1)
+        loss = loss + sum((forward(X(item,:),weight,bias,Func,k) - A*X(item,:)').^2);
+    end
+    fprintf('%.14f\n',loss);
+end
+
+
+
+
+function opt = forward(x,w,b,Func,k)
+    opt = w * activation(Func,x,3,k)' + b;
+end
+
+function [w,b] = backprob(w,b,x,y_h,y,Func,k)
+    lr = 0.01;
+    d_w = 2*(y_h - y)*activation(Func,x,3,k);
+    d_b = 2*(y_h - y);
+    w = w - lr*d_w;
+    b = b - lr*d_b;
+end
+
+
+function opt = activation(Func,inpt,c,k)
+if c == 1
+    opt = 2./(1+exp(-2*inpt))-1;
+end
+if c == 2 
+    opt = (exp(inpt) - exp(-inpt))./(exp(inpt)+exp(-inpt));
+end
+if c == 3
+    opt = Func.tanh2(inpt,k);  %k = 6
+end
+end
+
+
